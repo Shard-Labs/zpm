e276bc4fa0e3cb7f79fe504930b75f70dbae89ff
dark64 <darem966@gmail.com>
2020-05-19 19:02:52 +0200

    create: cleanup if something goes wrong

--- src/ops/create.rs
+++ src/ops/create.rs
@@ -11,62 +11,73 @@ pub fn create(matches: &ArgMatches) -> Result<(), String> {
 
     debug!("Creating project: {}", name);
 
-    let root = PathBuf::from(path).join(name.clone());
-    debug!("Creating project root directory: {}", root.display());
+    let project_root_path = PathBuf::from(path).join(name.clone());
+    debug!(
+        "Creating project root directory: {}",
+        project_root_path.display()
+    );
 
-    std::fs::create_dir_all(&root).map_err(|why| {
+    std::fs::create_dir_all(&project_root_path).map_err(|why| {
         format!(
             "Could not create project directory ({}): {}",
-            root.display(),
+            project_root_path.display(),
             why
         )
     })?;
 
-    let target_dir_path = root.clone().join(DEFAULT_TARGET_DIR);
+    let result = {
+        let target_dir_path = project_root_path.clone().join(DEFAULT_TARGET_DIR);
+        debug!("Creating target directory: {}", target_dir_path.display());
 
-    debug!("Creating target directory: {}", target_dir_path.display());
-    std::fs::create_dir(&target_dir_path).map_err(|e| {
-        format!(
-            "Could not create target directory ({}): {}",
-            target_dir_path.display(),
-            e
-        )
-    })?;
+        std::fs::create_dir(&target_dir_path).map_err(|e| {
+            format!(
+                "Could not create target directory ({}): {}",
+                target_dir_path.display(),
+                e
+            )
+        })?;
 
-    let source_dir_path = root.clone().join(DEFAULT_SOURCE_DIR);
+        let source_dir_path = project_root_path.clone().join(DEFAULT_SOURCE_DIR);
+        debug!("Creating source directory: {}", source_dir_path.display());
 
-    debug!("Creating source directory: {}", source_dir_path.display());
-    std::fs::create_dir(&source_dir_path).map_err(|why| {
-        format!(
-            "Could not create source directory ({}): {}",
-            source_dir_path.display(),
-            why
-        )
-    })?;
+        std::fs::create_dir(&source_dir_path).map_err(|why| {
+            format!(
+                "Could not create source directory ({}): {}",
+                source_dir_path.display(),
+                why
+            )
+        })?;
 
-    let config = Config::new(name.to_string());
+        let config = Config::new(name.to_string());
+        let config_path = project_root_path.clone().join("config.zcf");
+        let config_file = File::create(&config_path)
+            .map_err(|why| format!("Could not create {}: {}", config_path.display(), why))?;
 
-    let config_path = root.clone().join("config.zcf");
-    let config_file = File::create(&config_path)
-        .map_err(|why| format!("Could not create {}: {}", config_path.display(), why))?;
+        debug!("Writing: {}", config_path.display());
+        let writer = BufWriter::new(config_file);
+        config
+            .write(writer)
+            .map_err(|e| format!("Could not write to {}: {}", config_path.display(), e))?;
 
-    debug!("Writing: {}", config_path.display());
-    let writer = BufWriter::new(config_file);
-    config
-        .write(writer)
-        .map_err(|e| format!("Could not write to {}: {}", config_path.display(), e))?;
+        let entry_path = source_dir_path.clone().join(config.general.entry.as_str());
+        debug!("Writing: {}", entry_path.display());
 
-    let entry_path = source_dir_path.clone().join(config.general.entry.as_str());
+        std::fs::write(&entry_path, "def main() -> ():\n    return")
+            .map_err(|e| format!("Could not create {}: {}", entry_path.display(), e))?;
 
-    debug!("Writing: {}", entry_path.display());
-    std::fs::write(&entry_path, "def main() -> ():\n    return")
-        .map_err(|e| format!("Could not create {}: {}", entry_path.display(), e))?;
+        let gitignore = project_root_path.clone().join(".gitignore");
+        debug!("Writing: {}", gitignore.display());
 
-    let gitignore = root.clone().join(".gitignore");
+        std::fs::write(&gitignore, "target")
+            .map_err(|e| format!("Could not create {}: {}", entry_path.display(), e))?;
 
-    debug!("Writing: {}", gitignore.display());
-    std::fs::write(&gitignore, "target")
-        .map_err(|e| format!("Could not create {}: {}", entry_path.display(), e))?;
+        Ok(())
+    };
 
-    Ok(())
+    // cleanup if something went wrong while creating the project
+    if result.is_err() {
+        std::fs::remove_dir_all(&project_root_path).expect("Could not cleanup project");
+    }
+
+    result
 }
