61e222b919084bab393c3ab7f9d557cf51a01419
dark64 <darem966@gmail.com>
2020-06-01 13:14:53 +0200

    add test.sh

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+project=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 16)
+
+cd $tmp_dir
+
+zpm create $project
+cd $project
+
+zpm compile
+zpm compute
+zpm setup
+zpm export-verifier
+zpm generate-proof
+zpm verify
+
+rm -rf $tmp_dir
