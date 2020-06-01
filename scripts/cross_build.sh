e8e4c6e4d236ee6cfb43eaa2d7088e574842b243
dark64 <darem966@gmail.com>
2020-06-01 14:44:38 +0200

    cross build

---
+++ scripts/cross_build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#  x86_64-apple-darwin
+#  x86_64-pc-windows-gnu
+#  x86_64-unknown-linux-gnu
+
+set -e
+
+stage=$(mktemp -d -t cb-XXXXXXXXXX)
+src=$(pwd)
+tag=$(cat Cargo.toml | grep '^version' | awk '{print $3}' | sed -e 's/"//g')
+
+case $TARGET in
+    x86_64-pc-windows-gnu)
+        BINARY_NAME=zpm.exe
+        ;;
+    *)
+        BINARY_NAME=zpm
+        ;;
+esac
+
+echo TARGET=$TARGET
+echo TAG=$tag
+
+cross build --target $TARGET --release
+
+cp target/$TARGET/release/$BINARY_NAME $stage/
+cd $stage
+
+mkdir -p $src/artifacts
+tar czf $src/artifacts/zpm-$tag-$TARGET.tar.gz *
+
+cd $src
+rm -rf $stage
